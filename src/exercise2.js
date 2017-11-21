602e4779886825772abec652c6266f7075c8938b
GeoffreyYnov <geoffrey.moreau@ynov.com>
2017-11-21 17:19:58 +0100

    exercices

--- src/exercise2.js
+++ src/exercise2.js
@@ -1,3 +1,12 @@
-exports.run = function(input) {
-  console.log("Exercise #2");
+exports.run = function (input) {
+    console.log("Exercise #2");
+    var numbers = input.split("");
+    var aNumber = numbers[0];
+
+    numbers.forEach(element => {
+        if (element > aNumber) {
+            aNumber = element;
+        }
+    });
+    return parseInt(aNumber);
 };
